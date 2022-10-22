9eab99a7737e0efa45e477f17107e725ec94f0f7
Herlangga Wicaksono <herlangga.dev@gmail.com>
2022-10-22 08:32:53 +0700

    fix retrofit

--- app/src/main/java/com/gemastik/dentistsmile/data/network/ApiServiceDentist.java
+++ app/src/main/java/com/gemastik/dentistsmile/data/network/ApiServiceDentist.java
@@ -1,7 +1,9 @@
 package com.gemastik.dentistsmile.data.network;
 
+import com.chuckerteam.chucker.api.ChuckerInterceptor;
 import com.gemastik.dentistsmile.data.network.interceptor.AuthInterceptorDentist;
 import com.gemastik.dentistsmile.data.network.interceptor.AuthInterceptorStn;
+import com.gemastik.dentistsmile.root.App;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -18,6 +20,7 @@ public class ApiServiceDentist {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .addInterceptor(new AuthInterceptorDentist())
                 .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(new ChuckerInterceptor.Builder(App.instance).build())
                 .build();
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
